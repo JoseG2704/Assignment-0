c94ebdf0fe80156a682e618fff3f9f6f5d89e30d
JoseG2704 <joseg50274@gmail.com>
2025-06-15 22:20:47 -0400

    countOfAllNumbersSmallerThanTarget.js completed

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,12 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
   // Insert code here;
+  let count = 0;
+  for(let num of nums) {
+    if (num < target) {
+      count++;
+    }
+  }
+  return count;
 }
 
 // Do not edit this line;

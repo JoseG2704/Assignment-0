8ec41bbbd30bfd022474054b7506dcc8c924e09c
JoseG2704 <joseg50274@gmail.com>
2025-06-15 22:35:13 -0400

    fizzBuzz.js completed

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,18 @@
 function fizzBuzz(start, end) {
   // Insert code here;
+   const result = [];
+
+  for (let i = start; i <= end; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else if(i % 5 === 0) {
+      result.push("Buzz");
+    } else {
+      result.push(i);
+    }
+  }
 }
 
 // Do not edit this line;

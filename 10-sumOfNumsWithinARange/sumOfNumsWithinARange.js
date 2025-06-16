4f21fe9ce24db5692ec850f9445a8df590f878ad
JoseG2704 <joseg50274@gmail.com>
2025-06-15 22:29:20 -0400

    sumOfNumsWithinARange.js completed

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,12 @@
 function sumOfNumsWithinARange(nums, start, end) {
   // Insert code here;
+  let count = 0;
+  for(let num of nums) {
+    if (num >= start && num <= end) {
+      count++;
+    }
+  }
+  return count;
 }
 
 // Do not edit this line;

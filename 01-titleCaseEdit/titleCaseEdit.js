578ed1c8ddb8805f610531cd086fd109a407498f
JoseG2704 <joseg50274@gmail.com>
2025-06-15 22:03:34 -0400

    01 Changed/Completed

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,9 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  return title
+  .split('')
+  .map(word => word[0].toUpperCase() + word.slice(1).toUpperCase())
+  .join('')
 }
 
 // Do not edit this line;

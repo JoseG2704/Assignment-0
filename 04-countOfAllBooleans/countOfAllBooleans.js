adc50ccae4828767f4801dd2a6a518d169d09cad
JoseG2704 <joseg50274@gmail.com>
2025-06-15 22:15:43 -0400

    countOfAllBooleans.js completed

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,12 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+  let count = 0;
+  for(let item of arr) {
+    if(typeof item === "boolean") {
+      count++;
+    }
+  }
+  return count;
 }
 
 // Do not edit this line;

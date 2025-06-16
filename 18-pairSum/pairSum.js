a7cbee2bd44572ab0d49affec64b1bf7f19abe46
JoseG2704 <joseg50274@gmail.com>
2025-06-15 22:52:28 -0400

    pairSum.js completed

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,18 @@
 function pairSum(nums, target) {
   // Insert code here;
+  if(nums.length <= 1) {
+    throw new Error("Input array must have at least two elements.");
+  }
+
+  const seen = new Set();
+  for(const num of nums) {
+   const complement = target - num;
+   if(seen.has(complement)) {
+    return true;
+   }
+   seen.add(num);
+  }
+  return false;
 }
 
 // Do not edit this line;

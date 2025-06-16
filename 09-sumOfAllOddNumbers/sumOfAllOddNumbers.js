6a41e939c736a37b812cf4b65f9a0369de18ac3d
JoseG2704 <joseg50274@gmail.com>
2025-06-15 22:27:41 -0400

    sumOfAllOddNumbers.js completed

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,5 +1,12 @@
 function sumOfAllOddNumbers(nums) {
   // Insert code here;
+  let count = 0;
+  for (let num of nums) {
+    if (num % 2 !== 0) {
+      count++;
+    }
+  }
+  return count;
 }
 
 // Do not edit this line;

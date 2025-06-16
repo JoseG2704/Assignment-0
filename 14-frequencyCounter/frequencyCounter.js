57df9faa4cf461ee6a8d793c4d328d54febb8345
JoseG2704 <joseg50274@gmail.com>
2025-06-15 22:41:43 -0400

    frequencyCounter.js completed

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,15 @@
 function frequencyCounter(word) {
   // Insert code here;
+  const frequencies = {};
+
+  for(let char of word) {
+    if(frequencies[char]) {
+      frequencies[char]++;
+    } else {
+      frequencies[char] = 1;
+    }
+  }
+  return frequencies;
 }
 
 // Do not edit this line;

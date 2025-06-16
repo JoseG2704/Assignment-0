ec8f67dab3a22a7b339997ad6ab93cb65e67ce85
JoseG2704 <joseg50274@gmail.com>
2025-06-15 22:30:58 -0400

    sumOfMinimumAndMaximum.js completed

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,10 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+  const min = Math.min(...nums);
+
+  const max = Math.max(...nums);
+
+  return min + max;
 }
 
 // Do not edit this line;
